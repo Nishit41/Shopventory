2ba175ec891c1ab203a58829c6f174dcd238f4b2
nishit <nishitranjan41@gmail.com>
2024-11-23 18:05:52 +0530

    resolved Dashboard files conflict

--- src/Pages/Component/Dashboard.tsx
+++ src/Pages/Component/Dashboard.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../Component/Header';
 // import MyCartComponent from './Pages/Component/MyCartComponent';
-<<<<<<< HEAD
 import Books from './Books';
 import { Box } from '@mui/material';
 
@@ -11,16 +10,6 @@ function Dashboard() {
         <Header/>
        <Books/> 
    </Box>
-=======
-import BookComponent from '../Component/BookComponent';
-
-function Dashboard() {
-  return (
-    <div>
-        <Header/>
-       {/* <BookComponent/> */}
-   </div>
->>>>>>> 00c33b9e2a91c437811a09d54c293464bb4b0ca6
   )
 }
 
