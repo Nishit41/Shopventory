9b3b1c107283c8669b5379e25cda3cc83831ff87
nishit <nishitranjan41@gmail.com>
2024-12-15 18:33:06 +0530

    refctored this Dashborad

--- src/Component/Dashboard.tsx
+++ src/Component/Dashboard.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import Header from "./Header";
 import Books from "./Products";
 import { Box } from "@mui/material";
 import { Filter } from "../features/common/Filter";
@@ -7,7 +5,6 @@ import { Filter } from "../features/common/Filter";
 function Dashboard() {
   return (
     <Box>
-      <Header />
       <Filter />
       <Books />
     </Box>
--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -1,197 +1,55 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import { useState } from 'react';
-import { UserCart } from '../features/UserCart';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import Badge, { BadgeProps } from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useState } from "react";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-  
-    [theme.breakpoints.up('md')]: {
-      width: '50ch',
-     
+export const Header=()=> {
+   const [showCartItems, setShowCartItems] = useState<boolean>(false);
+  const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: "0 4px",
     },
-  },
-}));
-
-export default function Header() {
-  const [showCart,setShowCart] = useState<boolean>(false)
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-    React.useState<null | HTMLElement>(null);
-  const isMenuOpen = Boolean(anchorEl);
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
- const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
- const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-   const handleCart = () => {
-    setShowCart(true)
-   };
-
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  }));
 
   return (
-    <Box sx={{ flexGrow: 1,}}>
-      <AppBar position="fixed" style={{backgroundColor:"#A03037"}}>
-        <Toolbar>         
-           <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+     <>
+     <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
           >
-            
-          </Typography> 
-           <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block',} }}
-            >
-             <img src="" alt={"Product"} style={{width:"50px",height:"24px",marginLeft:"18vw"}}/>     
-          </Typography> 
-           <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block',marginLeft:"1vw" } }}
-          >
-            Bookstore 
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div">
+            <IconButton aria-label="cart" onClick={()=>setShowCartItems(true)}>
+              <StyledBadge badgeContent={4} color="secondary">
+                <ShoppingCartIcon />
+              </StyledBadge>
+            </IconButton>
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Box sx={{ flexGrow: 0.4 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge color="error" >
-                <Person2OutlinedIcon/>
-                 <div style={{fontSize:"xx-small",position:"absolute",top:"26px"}}>
-                 Profile
-                 </div>
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-             
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-             >
-               <Badge color="error"  onClick={()=>handleCart}>
-              < ShoppingCartOutlinedIcon />
-              <div style={{fontSize:"xx-small",position:"absolute",top:"26px",left:"2.5px"}}>
-               Cart
-              </div>
-              </Badge>
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
         </Toolbar>
       </AppBar>
-         {showCart && <UserCart/> }    
-    </Box>
+      {
+        showCartItems && (
+        <Box sx={{ width:"550px", background:"orange",color:"white" }}>
+          {/* Cart items */}
+          cdslkfjkdsflksfklflksdflsfkl
+        </Box>
+        )
+      }
+      </>
   );
 }
