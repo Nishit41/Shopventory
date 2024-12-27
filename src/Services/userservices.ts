29b139fe8152999b6eaa1962891bedbf8835a23d
nishit <nishitranjan41@gmail.com>
2024-12-27 23:59:07 +0530

    login

--- src/Services/userservices.ts
+++ src/Services/userservices.ts
@@ -1,16 +1,13 @@
 import axios from "axios";
 
 export const signInApi = async (signInObj: any) => {
-   let result = await fetch('https://fakestoreapi.com/auth/login',{
-        method:'POST',
-        body:JSON.stringify({
-            username: "mor_2314",
-            password: "83r5^_"
-        })
-    })
-
-    console.log("json",result);
-
+  let result = await fetch("https://fakestoreapi.com/auth/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: "mor_2314",
+      password: "83r5^_",
+    }),
+  });
   return await result.json();
 };
 export const signUpApi = (signupObject: any) => {
